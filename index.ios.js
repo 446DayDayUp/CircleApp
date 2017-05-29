2c8bb4caaf182524a3df56c1686cdf782205b506
Jing Tao <genetao06@hotmail.com>
2017-05-29 16:15:32 -0400

    Change app name to circle.

--- index.ios.js
+++ index.ios.js
@@ -12,7 +12,7 @@ import {
   View
 } from 'react-native';
 
-export default class chat extends Component {
+export default class circle extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -50,4 +50,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('chat', () => chat);
+AppRegistry.registerComponent('circle', () => circle);
