b0b4f7ac880760210d59fae7b49cae808ba6fa1f
z63zhu <z63zhu@outlook.com>
2017-06-09 17:42:21 -0400

    change the way to print Tag

--- android.src/component/ChatRoomPanel.js
+++ android.src/component/ChatRoomPanel.js
@@ -7,32 +7,33 @@ import {
     TouchableOpacity,
     Button,
 } from 'react-native';
+import MarqueeLabel from 'react-native-lahk-marquee-label';
 
 export default class ChatRoomPanel extends Component {
-    render() {
-        let room = this.props.room;
-       return(
+     render() {
+         let room = this.props.room;
+        return(
             <View key={room._id} style={{backgroundColor: '#F8F8F8'}}>
-              <TouchableOpacity activeOpacity={0.8}>
-                <View style={styles.container}>
-                  <View style={styles.left}>
-                      {this._chooseTag()}
-                  </View>
-                  <View style={styles.center}>
-                    <Text style={{fontSize: 16, color: '#546979'}}>
-                        {room.name} ({room.numUser})
-                    </Text>
-                    <Text style={{fontSize: 16, color: '#546979'}}>{room.tags[1]}</Text>
-                  </View>
-                  <View style={styles.right}>
-                    <Text style={{fontSize: 17, color: '#B07267'}}>{room.distance}m</Text>
-                    <Button style={{flex: 1}}
-                            color='gainsboro'
-                            title={this.props.btnText}
-                            onPress={this.props.btnHandler}/>
-                  </View>
-                </View>
-              </TouchableOpacity>
+                <TouchableOpacity activeOpacity={0.8}>
+                    <View style={styles.container}>
+                        <View style={styles.left}>
+                            {this._chooseTag()}
+                        </View>
+                        <View style={styles.center}>
+                            <Text style={{fontSize: 20, color: '#546979', flex: 2, marginTop: 10}}>
+                                {room.name} ({room.numUser})
+                            </Text>
+                            {this._showTag()}
+                        </View>
+                        <View style={styles.right}>
+                            <Text style={{fontSize: 17, color: '#B07267'}}>{room.distance}m</Text>
+                            <Button style={{flex: 1}}
+                                    color='darkturquoise'
+                                    title={this.props.btnText}
+                                    onPress={this.props.btnHandler}/>
+                        </View>
+                    </View>
+                </TouchableOpacity>
 
 
             </View>
@@ -42,15 +43,43 @@ export default class ChatRoomPanel extends Component {
 
     }
 
-on
+    _showTag() {
+        let PTags = [];
+        for(let i=0; i < this.props.room.tags.length; i++) {
+            PTags.push(
+                <Text style={{fontStyle: 'italic', fontSize: 12, color: '#546979'}}> #{this.props.room.tags[i]} </Text>
+            );
+        }
+        let tagStr = '';
+        for(let i=0; i < this.props.room.tags.length; i++) {
+            tagStr = tagStr + '#' + this.props.room.tags[i] + ' ';
+        }
+        if(tagStr.length < 31){
+            return (
+            <View style = {{flex: 1, marginBottom: 5, flexDirection: 'row'}}>
+                {PTags}
+            </View>
+            );
+        }else{
+            return (
+                <MarqueeLabel
+                duration={8000}
+                text={tagStr}
+                bgViewStyle = {{flex: 1, marginBottom: 5, flexDirection: 'row'}}
+                textStyle={{fontStyle: 'italic', fontSize: 12, color: '#546979'}}
+                />
+            );
+        }
+    }
+
     _chooseTag() {
-        if (this.props.room.tags[0] == 'food') {
+        if (this.props.room.tags[0] == 'Food') {
             return ( <Image style={styles.icon} source={require('../../img/food.png')}/>);
-        }else if (this.props.room.tags[0] == 'study') {
+        }else if (this.props.room.tags[0] == 'Study') {
             return (<Image style={styles.icon} source={require('../../img/study.png')}/>);
-        }else if (this.props.room.tags[0] == 'game') {
+        }else if (this.props.room.tags[0] == 'Game') {
             return (<Image style={styles.icon} source={require('../../img/game.png')}/>);
-        }else if (this.props.room.tags[0] == 'sports') {
+        }else if (this.props.room.tags[0] == 'Sports') {
             return (<Image style={styles.icon} source={require('../../img/sports.png')}/>);
         }else{
             return (<Image style={styles.icon} source={require('../../img/default.png')}/>);
@@ -95,6 +124,7 @@ var styles = StyleSheet.create({
     },
     center: {
         flex: 3,
+
         justifyContent: 'center',
     },
     right: {
