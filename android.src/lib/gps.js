3627ad8a23b2dc408822524ff702846323773e4c
Jing Tao <genetao06@hotmail.com>
2017-06-09 15:13:35 -0400

    Delete old code.

--- android.src/lib/gps.js
+++ android.src/lib/gps.js
@@ -3,25 +3,6 @@ import {
 } from 'react-native';
 var Geolocation  = NativeModules.Geolocation;
 
-// let getCords = (opt) => {
-//   opt = opt || fastMode;
-//   return new Promise((resolve) => {
-//     return navigator.geolocation.getCurrentPosition(function(location) {
-//       lat = location.coords.latitude;
-//       lng = location.coords.longitude;
-//       resolve({lat, lng});
-//     }, function(err) {
-//       if (lat === null || lng === null) {
-//         console.warn('Get GPS failed, retrying!!!');
-//         resolve(getCords());
-//       } else {
-//         // If timeout, use previous avaiblable GPS cords.
-//         resolve({lat, lng});
-//       }
-//     }, opt);
-//   })
-// };
-
 exports.getGpsCord = () => {
   return Geolocation.getCurrentLocation();
 }
