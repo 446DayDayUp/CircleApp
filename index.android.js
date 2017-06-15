4ba3ca7c31c10f82a759bad870be248260be2577
Ziyi Chen <zy9chen@uwaterloo.ca>
2017-06-15 17:28:14 -0400

    improve chat room ui and scroll to end

--- index.android.js
+++ index.android.js
@@ -15,7 +15,9 @@ import ChatRoom from './android.src/component/ChatRoom.js';
 import {
   AppRegistry,
 } from 'react-native';
-console.ignoredYellowBox = ['Setting a timer for a long']
+
+console.ignoredYellowBox = ['Setting a timer for a long'];
+
 export default class circle extends Component {
   render() {
     return (
