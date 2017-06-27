fdb2c3e8f24c3bb532e4cd72c5c8b127772e911a
Ziyi Chen <zy9chen@uwaterloo.ca>
2017-06-27 19:35:37 -0400

    Init image picker and add selector in the video and image icon in the chatroom

--- android.src/component/MoreView.js
+++ android.src/component/MoreView.js
@@ -1,7 +1,7 @@
 /**
  * Created by zachary on 2017-06-19.
  */
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import {
   StyleSheet,
@@ -10,33 +10,21 @@ import {
   Image,
   Dimensions,
   PixelRatio,
+  TouchableOpacity
 } from 'react-native';
 
-let { width } = Dimensions.get('window');
+let {width} = Dimensions.get('window');
 
-const icons = [
-  require('../../img/ic_more_gallery.png'),
-  require('../../img/ic_more_movie.png'),
-  require('../../img/ic_more_position.png'),
-  require('../../img/small_game.png'),
-];
+const icons = [require('../../img/ic_more_gallery.png'), require('../../img/ic_more_movie.png'), require('../../img/ic_more_position.png'), require('../../img/small_game.png')];
 
-const iconTexts = [
-  'photo', 'video', 'location', 'game',
-];
+const iconTexts = ['photo', 'video', 'location', 'game'];
 
 export default class MoreView extends Component {
   render() {
     let parts = [];
     for (let i = 0; i < 4; i++) {
-        parts.push(
-          <Cell
-            key={i}
-            icon={icons[i]}
-            text={iconTexts[i]}
-          />
-        );
-      }
+      parts.push(<Cell key={i} icon={icons[i]} text={iconTexts[i]}/>);
+    }
     return (
       <View style={styles.moreViewContainer}>
         {parts}
@@ -46,14 +34,49 @@ export default class MoreView extends Component {
 }
 
 class Cell extends Component {
+  _handlePress(type) {
+    if (type === 'photo' || type === 'video') {
+      var ImagePicker = require('react-native-image-picker');
+      var options = {
+        title: 'Select Avatar',
+        customButtons: [
+          {
+            name: 'fb',
+            title: 'Choose Photo from Facebook'
+          }
+        ],
+        storageOptions: {
+          skipBackup: true,
+          path: 'images'
+        }
+      };
+
+      ImagePicker.showImagePicker(options, (response) => {
+        console.log('Response = ', response);
+
+        if (response.didCancel) {
+          console.log('User cancelled image picker');
+        } else if (response.error) {
+          console.log('ImagePicker Error: ', response.error);
+        } else if (response.customButton) {
+          console.log('User tapped custom button: ', response.customButton);
+        } else {
+          let source = {
+            uri: response.uri
+          };
+          this.setState({avatarSource: source});
+        }
+      });
+    }
+  }
   render() {
     return (
-      <View style={styles.cellContainer}>
+      <TouchableOpacity style={styles.cellContainer} onPress={() => this._handlePress(this.props.text)}>
         <View style={styles.cellImgContainer}>
-          <Image style={styles.cellImage} source={this.props.icon} />
+          <Image style={styles.cellImage} source={this.props.icon}/>
         </View>
         <Text style={styles.cellText}>{this.props.text}</Text>
-      </View>
+      </TouchableOpacity>
     );
   }
 }
@@ -73,7 +96,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginLeft: 10,
-    marginRight: 10,
+    marginRight: 10
   },
   cellImgContainer: {
     width: 55,
@@ -83,13 +106,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#FBFBFB',
     borderWidth: 1 / PixelRatio.get(),
     borderColor: '#DFDFDF',
-    borderRadius: 10,
+    borderRadius: 10
   },
   cellImage: {
     width: 35,
-    height: 35,
+    height: 35
   },
   cellText: {
-    fontSize: 13,
+    fontSize: 13
   }
 });
