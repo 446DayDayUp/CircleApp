26857a0278c04ef338633e5caae1040768cd88b6
Ziyi Chen <zy9chen@uwaterloo.ca>
2017-07-18 18:39:57 -0400

    location ui

--- android.src/component/messages/LocationMessage.js
+++ android.src/component/messages/LocationMessage.js
@@ -81,11 +81,13 @@ export default class LocationMessage extends Component {
                   coordinate={{'latitude': msg.opt.lat, 'longitude': msg.opt.lng}}
                   title={msg.userName}
                   image={profilePicture[msg.iconName]}
+									style={styles.marker}
                 />
                 <MapView.Marker
                   coordinate={{'latitude': this.state.selfLat, 'longitude': this.state.selfLng}}
                   title={this.state.selfUsername}
                   image={profilePicture[this.state.selfIcon]}
+									style={styles.marker}
                 />
               </MapView>
             </View>
@@ -142,6 +144,7 @@ export default class LocationMessage extends Component {
                   coordinate={{'latitude': msg.opt.lat, 'longitude': msg.opt.lng}}
                   title={msg.userName}
                   image={profilePicture[msg.iconName]}
+									style={styles.marker}
                 />
     					</MapView>
             </View>
@@ -212,5 +215,8 @@ const styles = StyleSheet.create({
     bottom: 0,
     flex: 1,
   },
+	marker: {
+        width: 60,
+        height: 75,
+	},
 });
-
